c56e6326dc8adb26837f594324738adaaf368b07
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-03-16 23:07:36 +0900

    modify client

--- client/index.js
+++ client/index.js
@@ -1,3 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ({}) => {
+  return (
+    <div>
+      <h1>app</h1>
+    </div>
+  );
+}
+
 window.addEventListener('load', () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
   console.log('ready');
 });
