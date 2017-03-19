3f60b4815f21345a924e02ed79378039f6cc92d9
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-03-19 20:48:16 +0900

    modify entryService

--- routes/entry.js
+++ routes/entry.js
@@ -3,7 +3,7 @@ const route = new Router();
 const service = require('../service/entryService.js').getInstance();
 
 route.get('/', async (ctx, next) => {
-  const entries = await service.all();
+  const entries = await service.latest();
   ctx.body = JSON.stringify(entries);
 });
 
--- service/entryService.js
+++ service/entryService.js
@@ -14,6 +14,10 @@ const EntryService = (() => {
       const q = 'select * from entry;';
       return this.dbService.execQuery(q);
     }
+    latest() {
+      const q = 'SELECT * FROM `entry` ORDER BY `entry`.`id` DESC LIMIT 0, 10;';
+      return this.dbService.execQuery(q);
+    }
     find(tgtId) {
       const q = 'select * from entry where id =' + tgtId + ';';
       return this.dbService.execQuery(q);
