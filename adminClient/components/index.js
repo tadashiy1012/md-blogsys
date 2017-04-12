65aaf0832133275ebee7abffe7db83baa3d17fd9
tadashiy1012 <yama3ki.tada4@gmail.com>
2017-04-12 09:06:33 +0900

    add ResultMessage

--- adminClient/components/index.js
+++ adminClient/components/index.js
@@ -4,10 +4,12 @@ import HomeContents from './HomeContents';
 import WriteContents from './WriteContents';
 import EntriesContents from './EntriesContents';
 import SettingContents from './SettingContents';
+import ResultMessage from './ResultMessage';
 export {
   Link, ContentsSplitter,
   HomeContents,
   WriteContents,
   EntriesContents,
-  SettingContents
+  SettingContents,
+  ResultMessage
 };
